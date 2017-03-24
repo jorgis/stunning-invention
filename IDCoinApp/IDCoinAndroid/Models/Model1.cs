af28a4bf9e59225616b8651ee811fa718309f79c
danielemary <danielemary@hotmail.com>
2017-03-24 11:49:09 +0100

    Adding android app

---
+++ IDCoinApp/IDCoinAndroid/Models/Model1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace IDCoinAndroid
+{
+	public class Model1
+	{
+		public string Text { get; set; }
+	}
+}
---
+++ IDCoinApp/IDCoinAndroid/Resources/Resource.designer.cs
@@ -0,0 +1,60 @@
+﻿namespace IDCoinAndroid
+{
+
+
+	public partial class Resource
+	{
+
+		public partial class Attribute
+		{
+
+			private Attribute()
+			{
+			}
+		}
+
+		public partial class Drawable
+		{
+
+			// aapt resource value: 0x7f020000
+			public const int icon = 2130837504;
+
+			private Drawable()
+			{
+			}
+		}
+
+		public partial class Layout
+		{
+
+			// aapt resource value: 0x7f030000
+			public const int Main = 2130903040;
+
+			private Layout()
+			{
+			}
+		}
+
+		public partial class String
+		{
+
+			// aapt resource value: 0x7f040000
+			public const int app_name = 2130968576;
+
+			private String()
+			{
+			}
+		}
+
+		public partial class Id
+		{
+
+			// aapt resource value: 0x7f050000
+			public const int webView = 2131034112;
+
+			private Id()
+			{
+			}
+		}
+	}
+}
