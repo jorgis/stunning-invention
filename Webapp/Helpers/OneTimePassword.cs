5b18ff081798bb0aac7da3df84772844a4bd51ba
Frank Karlsen <frank.karlsen@skandiabanken.no>
2017-03-24 11:21:43 +0100

    missing file

---
+++ Webapp/Helpers/OneTimePassword.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class OneTimePassword
+{
+    static private string[] w1  = {"Liten", "Stor", "Mega", "Flott", "Tullete", "Kravstor", "Enkel"};
+    private static string[] w2  = {"Mann", "Dame", "Gutt", "Jente", "Hund", "Nabo", "Ansatt", "Katt", "Plante"};
+
+    public static string Phrase()
+    {
+        var s1 = w1[new Random().Next(w1.Length-1)];
+        var s2 = w2[new Random().Next(w2.Length-1)];
+        return $"{s1} {s2}";
+    }
+}
