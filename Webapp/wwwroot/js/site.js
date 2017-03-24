45936a5bb089cfd593efda4bf94242fb952032e3
Frank Karlsen <frank.karlsen@skandiabanken.no>
2017-03-24 10:09:11 +0100

    ikke kjør funksjonen før jeg sier kjør funksjonen

--- Webapp/wwwroot/js/site.js
+++ Webapp/wwwroot/js/site.js
@@ -1,16 +1,24 @@
 ﻿// Write your Javascript code.
 
-(function poll() {
+function poll() {
     setTimeout(function() {
         $.ajax({
             url: "/Bank/Status",
             type: "GET",
             success: function(data) {
                 console.log("polling");
+                if(data === '1')
+                {
+                    window.location.href = "/Authenticator/success";
+                }
+            },
+            error: function(data){
+                console.log("polling failed");
+                window.location.href = "/Authenticator/error"
             },
             dataType: "json",
             complete: poll,
             timeout: 2000
         })
     }, 5000);
-})();
+};
